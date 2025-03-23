1dfcddf3e7f25788867c6ad27ba6a09b50c716a3
zhn2605 <zzihan0805@gmail.com>
2025-03-22 20:22:04 -0400

    added vec3 and mat4 for shader

--- include/shader.hpp
+++ include/shader.hpp
@@ -5,7 +5,10 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Shader{
 public:
@@ -15,9 +18,11 @@ public:
     void use();
     void destroy();
 
-    void setBool(const std::string &name, bool value) const;
-    void setInt(const std::string &name, int value) const;
-    void setFloat(const std::string&name, float value) const;
+    void setBool(const std::string & name, bool value) const;
+    void setInt(const std::string & name, int value) const;
+    void setFloat(const std::string& name, float value) const;
+    void setUniformVec3(const std::string& name, const glm::vec3& value) const;
+    void setUniformMat4(const std::string& name, const glm::mat4x4& value) const;
 
 private:
     void checkCompileErrors(GLuint shader, std::string type);
--- src/shader.cpp
+++ src/shader.cpp
@@ -54,15 +54,22 @@ Shader::Shader(const std::string vertexPath, const std::string fragmentPath) {
 void Shader::use() { glUseProgram(ID); }
 void Shader::destroy() { glDeleteProgram(ID); }
 
-void Shader::setBool(const std::string &name, bool value) const {         
+void Shader::setBool(const std::string& name, bool value) const {         
     glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
 }
-void Shader::setInt(const std::string &name, int value) const { 
+void Shader::setInt(const std::string& name, int value) const { 
     glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
 }
-void Shader::setFloat(const std::string &name, float value) const { 
+void Shader::setFloat(const std::string& name, float value) const { 
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
-} 
+}
+void Shader::setUniformVec3(const std::string& name, const glm::vec3& value) const {
+    glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, glm::value_ptr(value));
+}
+
+void Shader::setUniformMat4(const std::string& name, const glm::mat4x4& value) const {
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+}
 
 void Shader::checkCompileErrors(GLuint shader, std::string type) {
     int success;
